3783eb75dcef9e7a416a0006ec45a68d548f84f2
Atikan Sinthawanone <atigun47@gmail.come>
2023-04-21 02:38:25 +0700

    update models.Product on views,templates

--- sales/badminton/views.py
+++ sales/badminton/views.py
@@ -4,11 +4,9 @@ from datetime import datetime
 from .forms import RegisterForm
 from django.contrib.auth import login
 from django.urls import reverse
+from .models import *
 
-all_products = [
-    {'id' : 1 , 'title' : 'name1' , 'price' : 155500},
-    {'id' : 2 , 'title' : 'name2' , 'price' : 400},
-    {'id' : 3 , 'title' : 'name3' , 'price' : 200},]
+
 # Create your views here.
 
 def Home(request) :
@@ -20,13 +18,14 @@ def About(request) :
 def Badminton(request, badminton_id) :
     one_product = None
     try :
-        one_product = [f for f in all_products if f['id'] == badminton_id][0]        
-    except IndexError :
-        print('error')
+        one_product = Product.objects.get(id = badminton_id)
+    except :
+        print("ไม่พบสินค้า")
     context = {'product' : one_product}
     return render(request,'badminton.html',context)
 
 def Badmintons(request) :
+    all_products = Product.objects.all().order_by('-instock')
     context = {'products' : all_products}
     return render(request,'badmintons.html', context)
 
@@ -44,3 +43,4 @@ def register(requst) :
     #GET
     context = {'form' : form}
     return render(requst, 'register.html',context)
+
