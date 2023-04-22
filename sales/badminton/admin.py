774839f1b0146521235b3300fbb44a0c2097c19f
tanakorn <starkingamer1@gmail.com>
2023-04-22 13:26:47 +0700

    add type racket in admin

--- sales/badminton/admin.py
+++ sales/badminton/admin.py
@@ -3,7 +3,7 @@ from .models import *
 
 # Register your models here.
 class BookProduct(admin.ModelAdmin):
-    list_display = ('id','instock','title', 'description', 'size','price','special_price')
+    list_display = ('id','instock','title', 'description', 'size','price','special_price','type_racket')
     list_display_links = ('id',)
     list_editable = ('title',)
     list_filter = ('title',)
