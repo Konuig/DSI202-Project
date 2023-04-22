0b8dcfc0d42e3114849db1324d5acf89d95e5bad
Atikan Sinthawanon <atigun47@gmail.com>
2023-04-22 13:11:24 +0700

    Update product.picture on templates

--- sales/badminton/models.py
+++ sales/badminton/models.py
@@ -9,5 +9,14 @@ class Product(models.Model) :
     special_price = models.DecimalField(max_digits=10,decimal_places=2,null=True,blank=True,default=False)
     quantity = models.IntegerField(default=1, null=True,blank=True)
     instock = models.BooleanField(default=True)
+    #CHOICE
+    class Type_racket_CHOICE(models.TextChoices) :
+        POWER_RACKET = "Power racket"
+        ALL_AROUND = "All around racket"
+        SPEED = "Speed racket"
+
+    type_racket = models.CharField(max_length=20,choices=Type_racket_CHOICE.choices,
+                  default = Type_racket_CHOICE.SPEED )
+
     #file
     picture = models.ImageField(upload_to='product',null=True,blank = True)
